456342229fa884b6dbb8a09376e0aec0921f5615
Ckyever Gaviola <ckyeverg@gmail.com>
2024-07-20 18:45:12 +1000

    Improve typewriter animation

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-let textBase = "I'm a ";
-let roles = ["software engineer", "problem solver"];
+let roles = ["software engineer", "problem solver", "programmer"];
 let roleIndex = 0;
 let i = 0;
 let reverse = false;
@@ -15,29 +14,29 @@ function typeWriter() {
   var textJitter = Math.floor(Math.random() * (70 - 45) + 45);
 
   if (reverse) {
-    if (document.getElementById("text").innerHTML.length > textBase.length) {
-      // We're still in the process of removing the job
+    if (document.getElementById("text").innerHTML.length > 0) {
+      // We're still in the process of deleting
       document.getElementById("text").innerHTML = document
         .getElementById("text")
         .innerHTML.slice(0, -1);
       setTimeout(typeWriter, textJitter);
     } else {
       // Deleting done, set next role and repeat
-      roleIndex = (roleIndex + 1) % 2;
+      roleIndex = (roleIndex + 1) % roles.length;
       reverse = false;
-      setTimeout(typeWriter, 1000);
+      setTimeout(typeWriter, 0);
     }
   } else {
-    if (i === (textBase + roles[roleIndex]).length) {
+    if (i === roles[roleIndex].length) {
       // Line is done wait and then reverse
-      i = textBase.length;
+      i = 0
       reverse = true;
       setTimeout(typeWriter, 2000);
     } else {
       // Write text like a typewriter
-      if (i < (textBase + roles[roleIndex]).length) {
+      if (i < roles[roleIndex].length) {
         document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + (
-          textBase + roles[roleIndex]
+          roles[roleIndex]
         ).charAt(i);
         i++;
         setTimeout(typeWriter, textJitter);
